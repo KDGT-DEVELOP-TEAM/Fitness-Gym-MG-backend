156745718f5928644b5626e81f3eb14686d05ec6
Kqn0n <taharacomeon.0504@gmail.com>
2025-11-09 03:52:00 +0900

    refactor: ディレクトリ構造の修正

---
+++ src/test/java/com/example/fitnessgym_mg/FitnessgymMgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fitnessgym_mg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitnessgymMgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
